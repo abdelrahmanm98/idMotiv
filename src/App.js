a07a18d79fb20331a5453036d2d65e54b581ccd5
abdelrahmanm98 <abdo62771@gmail.com>
2022-10-06 04:14:01 +0200

    Protecting Pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import AuthPage from './pages/AuthPage';
@@ -5,8 +6,10 @@ import Booking from './pages/BookingPage';
 import DashBoard from './pages/DashBoard';
 import Layout from './components/Layout/Layout';
 import NotFound from './pages/NotFound';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Switch>
@@ -14,12 +17,16 @@ function App() {
           <AuthPage />
           {/* <Booking /> */}
         </Route>
+        {authCtx.isLoggedIn && (
         <Route path='/dashboard'>
           <DashBoard />
         </Route>
-        <Route path='/booking'>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path='/booking'>
           <Booking />
         </Route>
+        )}
         <Route path='*'>
           <NotFound />
         </Route>
